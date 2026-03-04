bb3a05e39acbd183a224dcbe71ee33498c173019
Schliecking <brunocostaxxe@gmail.com>
2026-03-04 17:38:50 -0300

    Definição das páginas e mudanças nos ícones e nomenclatura

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -19,6 +19,11 @@ export default function TabLayout() {
   }}
 >
      <Tabs.Screen
+     /* página inicial (talvez a página do chat seja a página inicial), onde o usuário
+     vai ter acesso aos seus relatos resumidos de maneira visual, e poder usar indicadores,
+     como quão bem ele se sentiu durante o dia ou semana, quantas horas e como dormiu,
+     se se sentiu sozinho, ansioso, estressado
+     */
         name="index"
         options={{
           title: 'Home',
@@ -28,11 +33,40 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+      /*página que vai conter recomendações de leituras de mini-artigos sobre saúde mental
+      controle emocional, estudos sobre medicamentos, sessão de identifique-se (pessoas públicas
+      que também enfretam problemas mentais), recomendações de livros, programas, músicas e obras
+      de arte que abordem o tema.
+      */
+        name="leituras"
+        options={{
+          title: 'Leituras',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'book-sharp' : 'book-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+      /* página que vai conter a IA que vai fazer perguntas e registrar 
+      1. eventos estressantes (descrição do que aconteceu);
+      2. como a pessoa se sentiu
+      3. como a pessoa reagiu
+      4. como a pessoa se sentiu depois da reação*/
+        name="chat"
+        options={{
+          title: 'Chat',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'chatbubble-ellipses-sharp' : 'chatbubble-ellipses-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+      /** página para configurações, alteração de foto de perfil, tema claro ou escuro, notificações, etc*/
         name="configuracoes"
         options={{
           title: 'Configurações',
           tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+            <Ionicons name={focused ? 'settings-sharp' : 'settings-outline'} color={color} size={24}/>
           ),
         }}
       />
